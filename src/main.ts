4fd1a179ab9ebcb91879e4c6a6958fe5c9e55a98
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2024-06-25 19:58:32 +0100

    fixed issue with deployment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ENVIRONMENT } from './common/configs/environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(ENVIRONMENT.APP.PORT || 3000);
 }
 bootstrap();
