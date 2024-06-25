196a53f6f8e6464b6497c9ab8f3765c13e319dee
Ejiofor Celestine Chinaza <121381551+CyberVerse2@users.noreply.github.com>
2024-06-25 20:46:46 +0100

    fixed signup and login

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,52 @@
-import { NestFactory } from '@nestjs/core';
+import 'reflect-metadata';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as express from 'express';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filter/filter';
 import { ENVIRONMENT } from './common/configs/environment';
+import { ResponseTransformerInterceptor } from './common/interceptors/response.interceptor';
+
+// import { audioToText } from './common/utils/speechmatics';
+// import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  app.use(helmet());
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+  /**
+   * interceptors
+   */
+  app.useGlobalInterceptors(
+    new ResponseTransformerInterceptor(app.get(Reflector)),
+  );
+
+  /**
+   * Set global exception filter
+   */
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  /**
+   * Set global prefix for routes
+   */
+  app.setGlobalPrefix('/api/v1');
+
+  /**
+   *  Set global pipes
+   */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   await app.listen(ENVIRONMENT.APP.PORT || 3000);
 }
 bootstrap();
